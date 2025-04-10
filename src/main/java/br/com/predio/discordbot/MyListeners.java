0a4bec6025ad3c5a4c770450cdc57d5524fd5991
Pedro Henrique Brandão Santos <pedro080-henrique@hotmail.com>
2025-04-10 00:30:57 -0300

    Renomeando classe

--- src/main/java/br/com/predio/discordbot/MyListeners.java
+++ src/main/java/br/com/predio/discordbot/MyListeners.java
@@ -1,11 +1,20 @@
 package br.com.predio.discordbot;
 
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class PingPongListener extends ListenerAdapter {
+
+    private static String canalId;
+
+    static {
+        canalId = PropertiesRetriver.getProperties("discord.canal-id");
+    }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event)
@@ -20,6 +29,15 @@ public class PingPongListener extends ListenerAdapter {
         {
             MessageChannel channel = event.getChannel();
             channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
+        }
+    }
+
+    @Override
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+        TextChannel mensagem = event.getGuild().getTextChannelById(canalId);
+        if (mensagem != null) {
+            mensagem.sendMessage(event.getUser().getAsMention() + "kita dessa porra").queue();
         }
     }
 }
