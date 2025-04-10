ed350fc8037f02a69c6a67d71133283f42a83c28
Pedro Henrique Brandão Santos <pedro080-henrique@hotmail.com>
2025-04-10 00:36:11 -0300

    Adicionando onGuildMemberJoin

--- src/main/java/br/com/predio/discordbot/MyListeners.java
+++ src/main/java/br/com/predio/discordbot/MyListeners.java
@@ -1,6 +1,5 @@
 package br.com.predio.discordbot;
 
-import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
@@ -8,7 +7,7 @@ import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class PingPongListener extends ListenerAdapter {
+public class MyListeners extends ListenerAdapter {
 
     private static String canalId;
 
@@ -17,27 +16,24 @@ public class PingPongListener extends ListenerAdapter {
     }
 
     @Override
-    public void onMessageReceived(MessageReceivedEvent event)
-    {
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        TextChannel mensagem = event.getGuild().getTextChannelById(canalId);
+        if (mensagem != null) {
+            mensagem.sendMessage(event.getUser().getAsMention() + " kita dessa porra").queue();
+        }
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
         if (event.getAuthor().isBot()) return;
         // We don't want to respond to other bot accounts, including ourself
         Message message = event.getMessage();
         String content = message.getContentRaw();
         // getContentRaw() is an atomic getter
         // getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
-        if (content.equals("!ping"))
-        {
+        if (content.equals("testando")) {
             MessageChannel channel = event.getChannel();
-            channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
-        }
-    }
-
-    @Override
-    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
-        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
-        TextChannel mensagem = event.getGuild().getTextChannelById(canalId);
-        if (mensagem != null) {
-            mensagem.sendMessage(event.getUser().getAsMention() + "kita dessa porra").queue();
+            channel.sendMessage("testando").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
         }
     }
 }
